9e4d0e431b0b463c82f39449d95d1720b44b03af
platon <platonalekseevi6@gmail.com>
2026-03-23 12:53:01 +0300

    Add additional vector tests and implement getSize method in Vector class

--- main.cpp
+++ main.cpp
@@ -1,17 +1,57 @@
 #include "vector-top-it.h"
 #include <iostream>
 #include <cstddef>
+using topit::Vector;
 bool test1() {
-    using topit::Vector;
+
 
     Vector<int> v;
     return v.isEmpty();
 }
+bool test2() {
+
+
+    Vector<int> v;
+    return v.getSize() == 0;
+}
+bool test3() {
+    constexpr size_t size = 3ull;
+
+    Vector<int> v(size);
+    return v.getSize() == 3;
+}
+bool test4() {
+    constexpr size_t size = 3ull;
+    Vector<int> v(size);
+    try {
+        v.at(size);
+        return true;
+    } catch (...) {
+        return false;
+    }
+}
+bool test5() {
+    constexpr size_t size = 3ull;
+    Vector<int> v(size);
+    try {
+        v.at(size+1);
+        return false;
+    } catch (const std::out_of_range&) {
+        return true;
+    } catch (...) {
+        return false;
+    }
+}
+
 int main() {
     using test_t = bool (*)();
     using case_t = std::pair<test_t, const char*>;
     case_t tests[] = {
-        {test1, "Test 1: isEmpty() should return true for an empty vector."}
+        {test1, "Test 1: isEmpty() should return true for an empty vector."},
+        {test2, "Test 2: getSize() should return 0 for an empty vector."},
+        {test3, "Test 3: getSize() should return the correct size for a non-empty vector."},
+        {test4, "Test 4: at() should throw an exception when accessing out-of-bounds index."},
+        {test5, "Test 5: at() should throw std::out_of_range when accessing out-of-bounds index."}
     };
     std::cout << std::boolalpha;
     for (size_t i = 0; i < sizeof(tests) / sizeof(case_t); ++i) {
--- vector-top-it.h
+++ vector-top-it.h
@@ -5,12 +5,19 @@ namespace topit {
     template<class T>
     struct Vector {
         Vector();
+        explicit Vector(size_t s, const T& val);
         ~Vector();
 
         Vector(const Vector<T> &) = delete;
         Vector<T> &operator=(const Vector<T> &) = delete;
 
         bool isEmpty() const noexcept;
+        size_t getSize() const noexcept;
+
+        T& operator[](size_t id) noexcept;
+        const T& operator[](size_t id) const noexcept;
+        T& at(size_t id);
+        const T& at(size_t id) const;
 
         private:
          T* data_;
@@ -22,13 +29,31 @@ namespace topit {
     bool operator==(const Vector<T>& lhs, const Vector<T>& rhs);
 }
 template<class T>
+topit::Vector<T>::Vector(size_t size, const T& val) :
+data_(size ? new T[size] : nullptr),
+size_(size),
+capacity_(size)
+{}
+template<class T>
 bool topit::Vector<T>::isEmpty() const noexcept {
     return !size_;
+}
+template<class T>
+size_t topit::Vector<T>::getSize() const noexcept {
+    return size_;
 }
 template<class T>
 topit::Vector<T>::Vector() : data_(nullptr), size_(0), capacity_(0) {}
 template<class T>
 topit::Vector<T>::~Vector() {
     delete[] data_;
+template<class T>
+T& topit::Vector<T>::at(size_t id)  {
+    throw std::logic_error("bad idea");
 }
+template<class T>
+const T& topit::Vector<T>::at(size_t id) const {
+    throw std::logic_error("bad idea");
+}
+
 #endif
