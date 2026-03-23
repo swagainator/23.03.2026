bd1f614d90f992276064321ad62a7ac27d3b4c48
platon <platonalekseevi6@gmail.com>
2026-03-23 11:06:29 +0300

    Implement Vector constructor and destructor; add isEmpty method

--- vector-top-it.h
+++ vector-top-it.h
@@ -12,13 +12,22 @@ namespace topit {
         bool isEmpty() const noexcept;
 
         private:
-         T* data;
-         size_t size;
-         size_t capacity;
+         T* data_;
+         size_t size_;
+         size_t capacity_;
 
     };
     template<class T>
     bool operator==(const Vector<T>& lhs, const Vector<T>& rhs);
 }
-
-#endif //INC_23_03_2026_VECTOR_TOP_IT_H
+template<class T>
+bool topit::Vector<T>::isEmpty() const noexcept {
+    return false;
+}
+template<class T>
+topit::Vector<T>::Vector() : data_(nullptr), size_(0), capacity_(0) {}
+template<class T>
+topit::Vector<T>::~Vector() {
+    delete[] data_;
+}
+#endif
